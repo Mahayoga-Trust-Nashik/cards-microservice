57968d1c1d1757b8416cd44f583daa84ea5469d9
Nachiket Kamod <nachikamod@gmail.com>
2022-08-30 20:30:28 +0530

    Third commit: + Database connection change + Timezone change

--- Dockerfile
+++ Dockerfile
@@ -8,13 +8,15 @@ RUN go mod download
 RUN env CGO_ENABLED=0 go build -o /cards-microservice
 
 FROM alpine:latest
+RUN apk add --no-cache tzdata
+ENV TZ=Asia/Kolkata
 
 WORKDIR /
 
 COPY --from=builder /cards-microservice /cards-microservice
 
-EXPOSE 5300
+ENV MYSQL_CONNECTION="multinlh_appdb_user:@ppdbu\$er2022@tcp(162.241.85.150:3306)/multinlh_appdb?charset=utf8mb4&parseTime=True&loc=Local"
 
-ENV TZ="Asia/Kolkata"
+EXPOSE 5300
 
 ENTRYPOINT ["/cards-microservice"]
--- services/database.go
+++ services/database.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"log"
+	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -13,7 +14,7 @@ var (
 
 func ConnectDB() {
 
-	d, err := gorm.Open(mysql.Open("mahayoga_api:Ndk@1940knk@tcp(mahayoga-database:3306)/mahayoga_mobile?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
+	d, err := gorm.Open(mysql.Open(os.Getenv("MYSQL_CONNECTION")), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Error connecting with database : %v", err)
 	}
